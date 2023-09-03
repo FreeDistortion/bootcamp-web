86284c33efef332ca4d06bdd14dc628f459cd8ff
KT <clouderneering@gmail.com>
2023-09-03 14:01:46 +0900

    090323_web_backups

---
+++ clientweb/src/main/webapp/common/js/exam.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+ 
+ function test1(){
+	alert("AC/DC")
+	
+}
+function test2(){
+	confirm("Do you really think AC/DC's the greatest musician of Austrailia?'")
+}
+function test3(){
+	prompt("Then, who's that do you think about?")
+}
